534adba53f7a09d938ec5844ee36a51d2196e093
jbrande <rocketfreak@gmail.com>
2021-03-01 23:40:07 -0600

    Project 1 Submission

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PHSX815_Project1
 
+Needed dependencies: numpy, matplotlib
+
 This Project allows a user to simulate the effects of dice rolls of various magnitudes and fairnesses. The user can modify the simulation by setting values for several input parameters.
 
 The main program file is python/DiceRoll.py, which can be run with these input parameters:
@@ -14,8 +16,17 @@ The main program file is python/DiceRoll.py, which can be run with these input p
 
 -output:	(optional) the name of the output file to print to. if not given, the program will just print the results to stdout
 
-Random.py has been modified to sample numbers from a categorical distribution with given probabilities. Currently, there is no analysis code. Feel free to play around with this.
+Random.py has been modified to sample numbers from a categorical distribution with given probabilities.
 
-For best results, run the program from the top level directory as "python python/DiceRoll.py [params]"
 
-Needed dependencies: numpy, matplotlib
+After the dice are rolled, and the output saved to a file, the user can then plot the results of the simulations with python/DicePlot.py. This takes three input parameters:
+
+-input0:     (mandatory) H0: the name of the file which holds the rolled dice data")
+
+-input1:     (mandatory) H1: the name of the file which holds the rolled dice data")
+
+-comb:	 	 (mandatory) given this number of dice (< num total rolls) plot the distribution of the sums of those rolls")
+
+This will read in the JSON results from input0 and input1, and calculate the needed quantities and plot the various histograms: a histogram of the frequencies of the faces found from many single die rolls, the probabilities of the sums of grouped dice rolls, and the log-likelihood ratios for H0 and H1. 
+
+For best results, run the programs from the top level directory as "python python/program_name.py -params"
--- python/DicePlot.py
+++ python/DicePlot.py
@@ -11,65 +11,173 @@ import matplotlib.pyplot as plt
 # import our Random class from python/Random.py file
 sys.path.append(".")
 from python.Random import Random
-
+usecomb = False
 # main function for our coin toss Python code
 if __name__ == "__main__":
 	# if no args passed (need at least the input file), dump the help message
 	# if the user includes the flag -h or --help print the options
 	if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) == 1:
-		print ("Usage: %s [-seed number] [-Nrolls integer] [-Nsides integer] [-probs 'array of floats'] [-output string]" % sys.argv[0])
-		print ("-input:     (mandatory) the name of the file which holds the rolled dice data")
-		print("-plot_comb: 	(optional)  given some number of dice (< num total rolls) plot the distribution of the sums of those rolls")
+		print ("Usage: %s [-input0 string] [-input1 string] [-comb int]" % sys.argv[0])
+		print ("-input0:     (mandatory) H0: the name of the file which holds the rolled dice data")
+		print ("-input1:     (mandatory) H1: the name of the file which holds the rolled dice data")
+		print ("-comb:	 	 (mandatory) given this number of dice (< num total rolls) plot the distribution of the sums of those rolls")
 		print
 		sys.exit(1)
 
 	# read the user-provided arguments from the command line (if there)
-	if '-input' in sys.argv:
-		p = sys.argv.index('-input')
+	if '-input0' in sys.argv:
+		p = sys.argv.index('-input0')
 		try:
-			inputfile = sys.argv[p+1]
-			#print(inputfile)
+			input0 = sys.argv[p+1]
 		except IndexError as e:
-			print("Must pass input filename")
+			print("Must pass input0 filename")
+	else:
+		print("Must pass input0 filename")
 
-	if '-plot_comb' in sys.argv:
-		p = sys.argv.index('-plot_comb')
-		comb = int(sys.argv[p+1])
+	if '-input1' in sys.argv:
+		p = sys.argv.index('-input1')
+		try:
+			input1 = sys.argv[p+1]
+		except IndexError as e:
+			print("Must pass input1 filename")
+	else:
+		print("Must pass input1 filename")
 
-	jsondata = ""
-	with open(inputfile) as f:
-		jsondata = json.load(f)
+	if '-comb' in sys.argv:
+		p = sys.argv.index('-comb')
+		try:
+			comb = int(sys.argv[p+1])
+			usecomb = True
+		except IndexError as e:
+			print("Must pass number of dice to group")
+	else:
+		print("Must pass number of dice to group")
+
+
+	# load json data for both trials from files
+	res0 = ""
+	with open(input0) as f:
+		res0 = json.load(f)
 		f.close()
 
-
-	nsides = jsondata["nsides"]
-	nrolls = jsondata["nrolls"]
-	probs = jsondata["probs"]
-	rolls = jsondata["rolls"]
-
+	res1 = ""
+	with open(input1) as f:
+		res1 = json.load(f)
+		f.close()
 
 	# get split locations to calculate sums of dice combinations
-	inds = np.arange(0, len(rolls), comb)[1:]
+	inds0 = np.arange(0, len(res0["rolls"]), comb)[1:]
 
 	# split rolls into subarrays, and sum them
-	splits = np.split(rolls, inds)
-	sums = []
-	for s in splits:
-		sums.append(np.sum(s))
+	splits0 = np.split(res0["rolls"], inds0)
+	sums0 = []
+	for s in splits0:
+		sums0.append(np.sum(s))
 	#print(sums)
 
-	fig = plt.figure()
-	plt.hist(rolls, nsides)
-	plt.xlabel('Roll Values')
-	plt.ylabel('Frequency')
-	plt.title('Freq. of Dice Rolls')
-	plt.show()
-	fig.savefig("result_hist_uneven.jpg", dpi=180)
+	inds1 = np.arange(0, len(res1["rolls"]), comb)[1:]
 
-	fig = plt.figure()
-	plt.hist(sums, 100)
-	plt.xlabel('Sum')
-	plt.ylabel('Frequency')
-	plt.title('Sums of {} Dice'.format(comb))
+	# split rolls into subarrays, and sum them
+	splits1 = np.split(res1["rolls"], inds1)
+	sums1 = []
+	for s in splits1:
+		sums1.append(np.sum(s))
+	#print(sums)
+
+	# plot single roll histograms
+	fig, ax = plt.subplots(1, 2, figsize=(15, 6))#, sharey=True)
+	ax[0].hist(res0["rolls"], res0["nsides"], label="Die 0")
+	ax[1].hist(res1["rolls"], res1["nsides"], label="Die 1")
+	ax[0].set_xlabel('Roll Values')
+	ax[1].set_xlabel('Roll Values')
+	ax[0].set_ylabel('Frequency')
+	ax[0].legend()
+	ax[1].legend()
+	plt.suptitle("Single Die Roll Face Frequencies")
 	plt.show()
-	fig.savefig("sum_hist_uneven.jpg", dpi=180)
+	fig.savefig("plots/fair_unfair_hist.jpg", dpi=180)
+
+
+	if usecomb:
+		# plot grouped roll histograms
+		# for probability distributions, find freedman-diaconis bin width: https://stackoverflow.com/questions/33203645/how-to-plot-a-histogram-using-matplotlib-in-python-with-a-list-of-data
+		sums0 = np.array(sums0)
+		q25, q75 = np.percentile(sums0,[.25,.75])
+		bin_width0 = 2*(q75 - q25)*len(sums0)**(-1/3)
+		bins0 = round((sums0.max() - sums0.min())/bin_width0)
+
+		sums1 = np.array(sums1)
+		q25, q75 = np.percentile(sums1,[.25,.75])
+		bin_width1 = 2*(q75 - q25)*len(sums1)**(-1/3)
+		bins1 = round((sums1.max() - sums1.min())/bin_width1)
+
+		fig, ax = plt.subplots(1, 2, figsize=(15, 6), sharey=True)
+		ax[0].hist(sums0, bins0, density=True, stacked=True, label="Die 0")
+		ax[1].hist(sums1, bins1, density=True, stacked=True, label="Die 1")
+		ax[0].set_xlabel('Group Sum')
+		ax[1].set_xlabel('Group Sum')
+		ax[0].set_ylabel('Probability')
+		ax[0].legend()
+		ax[1].legend()
+		plt.suptitle('Sums of {} Dice'.format(comb))
+		plt.show()
+		fig.savefig("plots/fair_unfair_sum_hist.jpg", dpi=180)
+
+
+	# get log likelihood ratios
+	llrs0 = []
+	for sub in splits0:
+		llr0 = 0
+		for i in range(len(sub)):
+			if sub[i] == 1:
+				llr0 += np.log(res1["probs"][0] / res0["probs"][0])
+			if sub[i] == 2:
+				llr0 += np.log(res1["probs"][1] / res0["probs"][1])
+			if sub[i] == 3:
+				llr0 += np.log(res1["probs"][2] / res0["probs"][2])
+			if sub[i] == 4:
+				llr0 += np.log(res1["probs"][3] / res0["probs"][3])
+			if sub[i] == 5:
+				llr0 += np.log(res1["probs"][4] / res0["probs"][4])
+			if sub[i] == 6:
+				llr0 += np.log(res1["probs"][5] / res0["probs"][5])
+		llrs0.append(llr0)
+
+
+	llrs1 = []
+	for sub in splits1:
+		llr1 = 0
+		for i in range(len(sub)):
+			if sub[i] == 1:
+				llr1 += np.log(res1["probs"][0] / res0["probs"][0])
+			if sub[i] == 2:
+				llr1 += np.log(res1["probs"][1] / res0["probs"][1])
+			if sub[i] == 3:
+				llr1 += np.log(res1["probs"][2] / res0["probs"][2])
+			if sub[i] == 4:
+				llr1 += np.log(res1["probs"][3] / res0["probs"][3])
+			if sub[i] == 5:
+				llr1 += np.log(res1["probs"][4] / res0["probs"][4])
+			if sub[i] == 6:
+				llr1 += np.log(res1["probs"][5] / res0["probs"][5])
+		llrs1.append(llr1)
+
+	llrs0 = np.sort(llrs0)
+	llrs1 = np.sort(llrs1)
+
+	# get boundary for 95% confidence interval
+	alpha = 0.05
+	alpha_index = int(len(llrs0) - len(llrs0)*alpha)
+	#print(alpha_index)
+
+	# plot the LLR plots
+	fig = plt.figure()
+	plt.hist(llrs0, 100, label="P(lambda|H0)", density=True, stacked=True)
+	plt.axvline(llrs0[alpha_index], color="k", linestyle="--", label="alpha = 0.05")
+	plt.hist(llrs1, 100, label="P(lambda|H1)", density=True, stacked=True)
+	plt.xlabel("LLR")
+	plt.ylabel("Probability")
+	plt.title("Log-Likelihood Ratios")
+	plt.legend()
+	plt.show()
+	fig.savefig("plots/LLRs_unfair.jpg", dpi=180)
